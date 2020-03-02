296b48bb08374cf9177b0f78ef53b0e713d9a28e
LeeMyungJic <remge007@naver.com>
2020-03-02 16:50:30 +0900

    액티비티 화면전환
    액티비티 추가 시 매니페스트에 추가해주기 !

---
+++ 액티비티 화면전환/main/java/com/example/activity/SubActivity.java
@@ -0,0 +1,19 @@
+package com.example.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_sub);
+        TextView nameText = (TextView) findViewById(R.id.nameText);
+        Intent intent = getIntent();
+        nameText.setText(intent.getStringExtra("nameText".toString()));
+    }
+
+}
